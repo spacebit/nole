138b842111d2f1c38c5eade9f3e793d3be11e6b4
Max Andreev <maxick20@gmail.com>
2025-02-27 12:53:18 +0500

    connect nil account

--- packages/web/src/contexts/NilContext.tsx
+++ packages/web/src/contexts/NilContext.tsx
@@ -1,19 +1,28 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { HttpTransport, PublicClient } from "@nilfoundation/niljs";
-// import { useSecrets } from "./SecretsContext";
+import {
+  HttpTransport,
+  LocalECDSAKeySigner,
+  PublicClient,
+  SmartAccountV1,
+} from "@nilfoundation/niljs";
+import { useSecrets } from "./SecretsContext";
 
 interface NilContextType {
   client: PublicClient | undefined;
   setClient: (client: PublicClient) => void;
+  account: SmartAccountV1 | undefined;
+  setAccount: (account: SmartAccountV1) => void;
 }
 
 const NilContext = createContext<NilContextType | undefined>(undefined);
 
 const NilProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  // const { privateKey, walletAddress } = useSecrets();
+  const { privateKey, walletAddress } = useSecrets();
   const [client, setClient] = useState<PublicClient | undefined>(undefined);
+  const [account, setAccount] = useState<SmartAccountV1 | undefined>(undefined);
 
   useEffect(() => {
+    if (!privateKey || !walletAddress) return;
     const client = new PublicClient({
       transport: new HttpTransport({
         endpoint: process.env.NEXT_PUBLIC_RPC!,
@@ -21,11 +30,20 @@ const NilProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       shardId: 1,
     });
 
+    const signer = new LocalECDSAKeySigner({ privateKey });
+    const account = new SmartAccountV1({
+      client,
+      signer,
+      pubkey: signer.getPublicKey(),
+      address: walletAddress,
+    });
+
     setClient(client);
-  }, []);
+    setAccount(account);
+  }, [privateKey, walletAddress]);
 
   return (
-    <NilContext.Provider value={{ client, setClient }}>
+    <NilContext.Provider value={{ client, setClient, account, setAccount }}>
       {children}
     </NilContext.Provider>
   );
--- packages/web/src/contexts/SecretsContext.tsx
+++ packages/web/src/contexts/SecretsContext.tsx
@@ -1,51 +1,60 @@
 // Because there are no wallet extensions we store secrets in the local storage
 // All this code should be eliminated in the future
 
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import { Hex, isHexString } from "@nilfoundation/niljs";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface SecretsContextType {
-  privateKey: string;
-  walletAddress: string;
+  privateKey: Hex | undefined;
+  walletAddress: Hex | undefined;
   setSecrets: (privateKey: string, walletAddress: string) => void;
   clearSecrets: () => void;
 }
 
 const SecretsContext = createContext<SecretsContextType | undefined>(undefined);
 
-export const WalletProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [privateKey, setPrivateKey] = useState('');
-  const [walletAddress, setWalletAddress] = useState('');
+export const WalletProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [privateKey, setPrivateKey] = useState<Hex | undefined>(undefined);
+  const [walletAddress, setWalletAddress] = useState<Hex | undefined>(
+    undefined
+  );
 
   // On mount, restore wallet details from localStorage.
   useEffect(() => {
-    const storedPrivateKey = localStorage.getItem('wallet_private_key');
-    const storedWalletAddress = localStorage.getItem('wallet_address');
-    if (storedPrivateKey) {
+    const storedPrivateKey = localStorage.getItem("wallet_private_key");
+    const storedWalletAddress = localStorage.getItem("wallet_address");
+    if (isHexString(storedPrivateKey)) {
       setPrivateKey(storedPrivateKey);
     }
-    if (storedWalletAddress) {
+    if (isHexString(storedWalletAddress)) {
       setWalletAddress(storedWalletAddress);
     }
   }, []);
 
   // Update state and localStorage with wallet details.
   const setSecrets = (newPrivateKey: string, newWalletAddress: string) => {
-    setPrivateKey(newPrivateKey);
-    setWalletAddress(newWalletAddress);
-    localStorage.setItem('wallet_private_key', newPrivateKey);
-    localStorage.setItem('wallet_address', newWalletAddress);
+    setPrivateKey(isHexString(newPrivateKey) ? newPrivateKey : undefined);
+    setWalletAddress(
+      isHexString(newWalletAddress) ? newWalletAddress : undefined
+    );
+    localStorage.setItem("wallet_private_key", newPrivateKey);
+    localStorage.setItem("wallet_address", newWalletAddress);
   };
 
   // Clear wallet details from state and localStorage.
   const clearSecrets = () => {
-    setPrivateKey('');
-    setWalletAddress('');
-    localStorage.removeItem('wallet_private_key');
-    localStorage.removeItem('wallet_address');
+    setPrivateKey(undefined);
+    setWalletAddress(undefined);
+    localStorage.removeItem("wallet_private_key");
+    localStorage.removeItem("wallet_address");
   };
 
   return (
-    <SecretsContext.Provider value={{ privateKey, walletAddress, setSecrets, clearSecrets }}>
+    <SecretsContext.Provider
+      value={{ privateKey, walletAddress, setSecrets, clearSecrets }}
+    >
       {children}
     </SecretsContext.Provider>
   );
@@ -54,7 +63,7 @@ export const WalletProvider: React.FC<{ children: React.ReactNode }> = ({ childr
 export const useSecrets = (): SecretsContextType => {
   const context = useContext(SecretsContext);
   if (!context) {
-    throw new Error('useWallet must be used within a WalletProvider');
+    throw new Error("useWallet must be used within a WalletProvider");
   }
   return context;
 };
